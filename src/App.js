ed20a53cd0a4718256ffa3b282d9907759cc9e6a
liuqiang <liuqiang1357@163.com>
2021-07-22 20:43:34 +0800

    add neo-dapi demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { NeoDapi } from '@neongd/neo-dapi';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [dapi, setDapi] = useState(null);
+
+  useEffect(() => {
+    const dapi = window.neo && new NeoDapi(window.neo);
+    setDapi(dapi);
+  }, []);
+
+
+  async function getProvider() {
+    alert(JSON.stringify(await dapi.getProvider()));
+  }
+
+  return dapi ? (
+    <div>
+      <button onClick={getProvider}>getProvider</button>
+    </div>
+  ) : (
+    <div>
+      Dapi Provider is not available.
     </div>
   );
 }
