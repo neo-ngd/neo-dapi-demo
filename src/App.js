2660f136ac3e9e293a046fd9d65fb9858cef5485
liuqiang <liuqiang1357@163.com>
2021-08-05 17:51:09 +0800

    add dapi test case

--- src/App.js
+++ src/App.js
@@ -7,17 +7,32 @@ function App() {
 
   useEffect(() => {
     const dapi = window.neo && new NeoDapi(window.neo);
+    // const dapi = new NeoDapi('http://seed1t4.neo.org:20332');
     setDapi(dapi);
   }, []);
 
-
   async function getProvider() {
-    alert(JSON.stringify(await dapi.wallet.getProvider()));
+    console.log(await dapi.wallet.getProvider());
+  }
+
+  async function getAccount() {
+    console.log(await dapi.wallet.getAccount());
+  }
+
+  async function getBlockHeight() {
+    console.log(await dapi.wallet.getBlockHeight({}));
+  }
+
+  async function getBlock() {
+    console.log(await dapi.node.getBlock(1, true));
   }
 
   return dapi ? (
     <div>
       <button onClick={getProvider}>getProvider</button>
+      <button onClick={getAccount}>getAccount</button>
+      <button onClick={getBlockHeight}>getBlockHeight</button>
+      <button onClick={getBlock}>getBlock</button>
     </div>
   ) : (
     <div>
