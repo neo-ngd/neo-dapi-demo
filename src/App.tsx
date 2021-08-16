e98b6fdb5ea8ede6e3373546c4bd270fa2abbf61
liuqiang <liuqiang1357@163.com>
2021-08-16 19:49:11 +0800

    update dependencies and add testcase

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   const [dapi, setDapi] = useState<NeoDapi | null>(null);
 
   useEffect(() => {
-    // const dapi = new NeoDapi('http://seed1.neo.org:10332');
+    // const dapi = new NeoDapi("http://seed1.neo.org:10332");
     const dapi = window.neo ? new NeoDapi(window.neo) : null;
     setDapi(dapi);
   }, []);
@@ -46,11 +46,78 @@ function App() {
 
   async function getApplicationLog() {
     // console.log(await dapi?.node.getApplicationLog("0xc68aac4b0bb9e88bd42086c50cebe648ad28726d2849ff73faeb93985e510587"));
-    console.log(await dapi?.wallet.getApplicationLog({txid: "0xc68aac4b0bb9e88bd42086c50cebe648ad28726d2849ff73faeb93985e510587"}));
+    console.log(
+      await dapi?.wallet.getApplicationLog({
+        txid: "0xc68aac4b0bb9e88bd42086c50cebe648ad28726d2849ff73faeb93985e510587",
+      })
+    );
   }
 
   async function getTransaction() {
-    console.log(await dapi?.wallet.getTransaction({txid: "0xc68aac4b0bb9e88bd42086c50cebe648ad28726d2849ff73faeb93985e510587"}));
+    // console.log(await dapi?.node.getRawTransaction("0xc68aac4b0bb9e88bd42086c50cebe648ad28726d2849ff73faeb93985e510587", true));
+    console.log(
+      await dapi?.wallet.getTransaction({
+        txid: "0xc68aac4b0bb9e88bd42086c50cebe648ad28726d2849ff73faeb93985e510587",
+      })
+    );
+  }
+
+  async function invokeRead() {
+    console.log(
+      await dapi?.wallet.invokeRead({
+        scriptHash: "0xef4073a0f2b305a38ec4050e4d3d28bc40ea63f5",
+        operation: "balanceOf",
+        args: [
+          {
+            type: "Hash160",
+            value: "0x6835f6961eadbad3e75f2ea2f7a52d04deb82005",
+          },
+        ],
+        network: "TestNet",
+      })
+    );
+  }
+
+  async function invokeReadMulti() {
+    console.log(
+      await dapi?.wallet.invokeReadMulti({
+        invokeArgs: [
+          {
+            scriptHash: "0xef4073a0f2b305a38ec4050e4d3d28bc40ea63f5",
+            operation: "balanceOf",
+            args: [
+              {
+                type: "Hash160",
+                value: "0x6835f6961eadbad3e75f2ea2f7a52d04deb82005",
+              },
+            ],
+          },
+          {
+            scriptHash: "0xd2a4cff31913016155e38e474a2c06d08be276cf",
+            operation: "balanceOf",
+            args: [
+              {
+                type: "Hash160",
+                value: "0x6835f6961eadbad3e75f2ea2f7a52d04deb82005",
+              },
+            ],
+          },
+        ],
+        network: "TestNet",
+      })
+    );
+  }
+
+  async function getBalance() {
+    console.log(
+      await dapi?.wallet.getBalance({
+        address: "NLP5mHikEuxyPCFqMCBHeP1YDyYPwCKBFu",
+        assetHashes: [
+          "0xef4073a0f2b305a38ec4050e4d3d28bc40ea63f5",
+          "0xd2a4cff31913016155e38e474a2c06d08be276cf",
+        ],
+      })
+    );
   }
 
   return dapi ? (
@@ -63,6 +130,9 @@ function App() {
       <button onClick={getBlock}>getBlock</button>
       <button onClick={getApplicationLog}>getApplicationLog</button>
       <button onClick={getTransaction}>getTransaction</button>
+      <button onClick={invokeRead}>invokeRead</button>
+      <button onClick={invokeReadMulti}>invokeReadMulti</button>
+      <button onClick={getBalance}>getBalance</button>
     </div>
   ) : (
     <div>Dapi Provider is not available.</div>
