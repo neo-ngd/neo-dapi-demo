027dbdda172d63f7cf55bab65f9b80e9f93430ad
xie-yue <disix77@gmail.com>
2021-10-08 16:49:56 +0800

    test for invoke

--- src/App.tsx
+++ src/App.tsx
@@ -121,12 +121,11 @@ function App() {
         signers: [
           {
             account: "0x96d5942028891de8e5d866f504b36ff5ae13ab63",
-            scopes: "1",
-            // allowedContracts: [
-            //   "590635eaf2364ba03dade7ed9a54efa20d72eba9",
-            //   "b137c83610d3f0331a48d8d6283864120b4f23a1",
-            //   "ef4073a0f2b305a38ec4050e4d3d28bc40ea63f5",
-            // ],
+            scopes: "CalledByEntry,CustomContracts",
+            allowedContracts: [
+              "2bcc9c9ad6626396f507f088c5ae06ebf6fa5efa",
+              "ef4073a0f2b305a38ec4050e4d3d28bc40ea63f5",
+            ],
           },
         ],
         network: "TestNet",
